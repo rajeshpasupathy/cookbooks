e08d113445b4f13b083ff30b16235354c19f363d
rajeshpasupathy <rajeshpkanna@gmail.com>
2016-01-17 17:15:31 -0600

    raj test added - 01/17

---
+++ rajtest/metadata.rb
@@ -0,0 +1,7 @@
+name 'rajtest'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures rajtest'
+long_description 'Installs/Configures rajtest'
+version '0.1.0'
---
+++ rajtest/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: rajtest
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+directory '/test1' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
