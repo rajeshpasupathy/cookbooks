2ff8cd96cc03374d1e097fbad7088bd83b6cc1fe
rajeshpasupathy <rajeshpkanna@gmail.com>
2016-01-25 21:48:49 -0600

    updated replace command

--- oracle11g_prov/recipes/oracle_user_config.rb
+++ oracle11g_prov/recipes/oracle_user_config.rb
@@ -105,7 +105,7 @@ end
 ruby_block "disabiling tty in /etc/sudoers" do
   block do
     fe = Chef::Util::FileEdit.new("/etc/sudoers")
-    fe.search_file_replace("Defaults requiretty", "#Defaults requiretty")
+    fe.search_file_replace("Defaults    requiretty", "#Defaults requiretty")
     fe.write_file
   end  
 end
